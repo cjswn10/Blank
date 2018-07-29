d53e3067afff2b0721d4b792f459c58ef447a561
yeonju105 <chewon10@hanmail.net>
2018-07-30 03:01:20 +0900

    18.07.30 03:00] 그림판 ajax test 젭알....

--- Blank/src/main/webapp/resources/js/drawingColor.js
+++ Blank/src/main/webapp/resources/js/drawingColor.js
@@ -1,6 +1,6 @@
-var colorName = ["maroon","red","orange","yellow","olive",
-				 "purple","fuchsia","white","lime","green",
-				 "navy","aqua","teal","black","silver","gray"];
+var colorName = ["c1","c2","c3","c4","c5",
+				 "c6","c7","c8","c9","c10",
+				 "c11","c12","c13","c14","c15","c16"];
 	for(var i=0;i<16;i++) {
 		document.write("<input type='button' id='"+ colorName[i] +"' onclick='cg_color(this.id)'>");
 	}
