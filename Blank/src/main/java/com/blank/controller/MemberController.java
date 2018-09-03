7e0fa60bd0b81ead774d473f5abfa84946f37731
1027 <1027@hb3015>
2018-09-03 11:39:49 +0900

    09/13 오류 수정

--- Blank/src/main/java/com/blank/controller/MemberController.java
+++ Blank/src/main/java/com/blank/controller/MemberController.java
@@ -37,7 +37,7 @@ public class MemberController {
 
 	
 
-	//筌띾뜆�뵠占쎈읂占쎌뵠筌욑옙
+	//嶺뚮씭�쐠占쎈턄�뜝�럥�쓡�뜝�럩逾좂춯�쉻�삕
 	@RequestMapping(value="/member/myPage.do")
 	public ModelAndView myPage() {
 		
@@ -46,7 +46,7 @@ public class MemberController {
 	}
 	
 
-	//�④쑴�젟筌≪뼐由�
+	//占썩몿�뫒占쎌젧嶺뚢돦堉먪뵳占�
 	@RequestMapping(value="search.do")
 	public ModelAndView search() {
 			
@@ -56,7 +56,7 @@ public class MemberController {
 	
 
 
-	//id筌≪뼐由�
+	//id嶺뚢돦堉먪뵳占�
 	@RequestMapping(value="searchIdPage.do")
 	public ModelAndView searchId() {
 				
@@ -64,7 +64,7 @@ public class MemberController {
 		return mav;
 	}
 	
-	//�뜮袁⑨옙甕곕뜇�깈 筌≪뼐由�
+	//占쎈쑏熬곣뫅�삕�뵓怨뺣쐡占쎄퉰 嶺뚢돦堉먪뵳占�
 	@RequestMapping(value="searchPwdPage.do")
 	public ModelAndView searchPwd() {
 					
@@ -72,7 +72,7 @@ public class MemberController {
 		return mav;
 	}
 
-	//占쎈툡占쎌뵠占쎈탵 筌≪뼐由�
+	//�뜝�럥�닡�뜝�럩逾졾뜝�럥�꺏 嶺뚢돦堉먪뵳占�
 	@RequestMapping(value="searchId.do")
 	@ResponseBody
 	public String searchId(String name,String phone)
@@ -93,7 +93,7 @@ public class MemberController {
 		return str;
 	}
 	
-	//�뜮袁⑨옙甕곕뜇�깈 筌≪뼐由�
+	//占쎈쑏熬곣뫅�삕�뵓怨뺣쐡占쎄퉰 嶺뚢돦堉먪뵳占�
 	@RequestMapping(value="searchPwd.do")
 	@ResponseBody
 	public String searchPwd(String id,String phone)
@@ -114,7 +114,7 @@ public class MemberController {
 		return str;
 	}
 	
-	//�눧紐꾩벥占쎄텢占쎈퉮(contact)
+	//占쎈닱筌뤾쑴踰ε뜝�럡�뀬�뜝�럥�돫(contact)
 	@RequestMapping(value="/member/qNa.do")
 	public ModelAndView Qna() {
 		
@@ -123,7 +123,7 @@ public class MemberController {
 	}
 
 
-	//嚥≪뮄�젃占쎈툡占쎌뜍
+	//�슖�돦裕꾬옙�쟽�뜝�럥�닡�뜝�럩�쐨
 	@RequestMapping(value="/member/logOut.do")
 	public ModelAndView logOut(HttpSession session,LogVo l,HttpServletRequest request) {
 		
@@ -131,8 +131,42 @@ public class MemberController {
 		
 		try {
 			
-			InetAddress addr = InetAddress.getLocalHost();
-			String ip = addr.getHostAddress();
+			String ip = request.getHeader("X-FORWARDED-FOR");
+			//System.out.println("TEST : X-FORWARDED-FOR : "+ip);
+
+			if (ip == null) {
+
+				ip = request.getHeader("Proxy-Client-IP");
+				//System.out.println("TEST : Proxy-Client-IP : "+ip);
+
+			}
+
+			if (ip == null) {
+
+				ip = request.getHeader("WL-Proxy-Client-IP");
+				//System.out.println("TEST : WL-Proxy-Client-IP : "+ip);
+
+			}
+
+			if (ip == null) {
+
+				ip = request.getHeader("HTTP_CLIENT_IP");
+				//System.out.println("TEST : HTTP_CLIENT_IP : "+ip);
+
+			}
+
+			if (ip == null) {
+
+				ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+				//System.out.println("TEST : HTTP_X_FORWARDED_FOR : "+ip);
+
+			}
+
+			if (ip == null) {
+
+				ip = request.getRemoteAddr();
+
+			}
 			
 			l.setIp(ip);
 			
@@ -143,7 +177,8 @@ public class MemberController {
 		
 		String id = request.getParameter("id");
 		
-		l.setId("["+id+"] 님이 로그아웃 하였습니다.");
+		l.setId(id);
+		l.setRecord("로그아웃");
 		
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss");
 
@@ -164,13 +199,14 @@ public class MemberController {
 			mav.addObject("msg", "실패");
 			mav.setViewName("error");
 		}
+
 		session.invalidate();
 		mav.setViewName("redirect:/login.do");
 		return mav;
 	}
 	
 
-	//占쎌돳占쎌뜚揶쏉옙占쎌뿯
+	//�뜝�럩�뤂�뜝�럩�쐸�뤆�룊�삕�뜝�럩肉�
 	@RequestMapping(value="join.do", method=RequestMethod.GET)	
 	public void joinForm() {
 		
@@ -183,14 +219,14 @@ public class MemberController {
 		int re = dao.memberInsert(mv);
 		if (re < 1) {
 
-			mav.addObject("msg", "占쎌돳占쎌뜚 揶쏉옙占쎌뿯 占쎈뼄占쎈솭");
+			mav.addObject("msg", "�뜝�럩�뤂�뜝�럩�쐸 �뤆�룊�삕�뜝�럩肉� �뜝�럥堉꾢뜝�럥�넮");
 			mav.setViewName("/member/error");
 		}
 		return mav;
 	}
 	
 
-	//占쎈툡占쎌뵠占쎈탵 餓λ쵎�궗占쎌넇占쎌뵥
+	//�뜝�럥�닡�뜝�럩逾졾뜝�럥�꺏 繞벿살탮占쎄텢�뜝�럩�꼪�뜝�럩逾�
 	@RequestMapping(value="checkId.do")
 	@ResponseBody
 	public String checkId(@RequestParam("id")String id) {
@@ -216,14 +252,14 @@ public class MemberController {
 	}
 	
 
-	//嚥≪뮄�젃占쎌뵥
+	//�슖�돦裕꾬옙�쟽�뜝�럩逾�
 	@RequestMapping(value="login.do", method=RequestMethod.GET)
 	public void loginForm() {
 		
 	}
 	
 	@RequestMapping(value="login.do", method=RequestMethod.POST)
-	public ModelAndView login(String id, String pwd, HttpSession session,LogVo l) {
+	public ModelAndView login(String id, String pwd, HttpSession session,LogVo l,HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView();
 		
 		Map map = new HashMap();
@@ -233,24 +269,59 @@ public class MemberController {
 		Boolean r = dao.login(map);
 		if (r == true) {
 
-			//id, 占쎌돳占쎌뜚甕곕뜇�깈 占쎄쉭占쎈�� 占쎄문占쎄쉐
+			//id, mno 세션생성
 			session.setAttribute("id", id);
 			session.setAttribute("mno", dao.mno(map));
 			mav.setViewName("redirect:/member/main.do");
 		}
 		try {
 			
-			InetAddress addr = InetAddress.getLocalHost();
-			String ip = addr.getHostAddress();
+			String ip = request.getHeader("X-FORWARDED-FOR");
+			//System.out.println("TEST : X-FORWARDED-FOR : "+ip);
+
+			if (ip == null) {
+
+				ip = request.getHeader("Proxy-Client-IP");
+				//System.out.println("TEST : Proxy-Client-IP : "+ip);
+
+			}
+
+			if (ip == null) {
+
+				ip = request.getHeader("WL-Proxy-Client-IP");
+				//System.out.println("TEST : WL-Proxy-Client-IP : "+ip);
+
+			}
+
+			if (ip == null) {
+
+				ip = request.getHeader("HTTP_CLIENT_IP");
+				//System.out.println("TEST : HTTP_CLIENT_IP : "+ip);
+
+			}
+
+			if (ip == null) {
+
+				ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+				//System.out.println("TEST : HTTP_X_FORWARDED_FOR : "+ip);
+
+			}
+
+			if (ip == null) {
+
+				ip = request.getRemoteAddr();
+
+			}
 			
 			l.setIp(ip);
-			
+			     
 		}catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e.getMessage());
 		}
 		
-		l.setId("["+id+"] 님이 로그인 하였습니다.");
+		l.setId(id);
+		l.setRecord("로그인");
 		
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss");
 
@@ -272,11 +343,13 @@ public class MemberController {
 			mav.setViewName("error");
 		}
 		
-		//mav.setViewName("redirect:/member/main.do");
+		//로그인 하고 3시간5분 동안 페이지를 이동하지않으면 세션 삭제(즉, 자동 로그아웃)
+		session.setMaxInactiveInterval(185*60);
+		
 		return mav;
 	}
 
-	//占쎌돳占쎌뜚占쎌젟癰귨옙 占쎈땾占쎌젟 占쎈뻻 �뜮袁⑨옙甕곕뜇�깈 占쎌넇占쎌뵥
+	//�뜝�럩�뤂�뜝�럩�쐸�뜝�럩�젧�솻洹⑥삕 �뜝�럥�빢�뜝�럩�젧 �뜝�럥六� 占쎈쑏熬곣뫅�삕�뵓怨뺣쐡占쎄퉰 �뜝�럩�꼪�뜝�럩逾�
   @RequestMapping(value="/member/pwdCheck.do", method=RequestMethod.GET)
 	public void pwdCheckForm() {
 		
@@ -299,14 +372,14 @@ public class MemberController {
 		else
 		{
 
-			mav.addObject("msg", "�뜮袁⑨옙甕곕뜇�깈揶쏉옙 占쎌뵬燁살꼹釉�筌욑옙 占쎈륫占쎈뮸占쎈빍占쎈뼄.");
+			mav.addObject("msg", "占쎈쑏熬곣뫅�삕�뵓怨뺣쐡占쎄퉰�뤆�룊�삕 �뜝�럩逾х뇖�궡瑗백뇡占썹춯�쉻�삕 �뜝�럥瑜ュ뜝�럥裕멨뜝�럥鍮띶뜝�럥堉�.");
 		}	
 		
 		return mav;
 	}
 	
 
-	//占쎌돳占쎌뜚占쎌젟癰귨옙 占쎈땾占쎌젟
+	//�뜝�럩�뤂�뜝�럩�쐸�뜝�럩�젧�솻洹⑥삕 �뜝�럥�빢�뜝�럩�젧
 	@RequestMapping(value="/member/updateMember.do", method=RequestMethod.GET)
 	public void memberUpdateForm() {
 		
@@ -319,14 +392,14 @@ public class MemberController {
 		
 		int re = dao.updateMember(mv);
 		if (re < 1) {
-			mav.addObject("msg", "占쎌돳占쎌뜚占쎌젟癰귨옙 占쎈땾占쎌젟 占쎈뼄占쎈솭");
+			mav.addObject("msg", "�뜝�럩�뤂�뜝�럩�쐸�뜝�럩�젧�솻洹⑥삕 �뜝�럥�빢�뜝�럩�젧 �뜝�럥堉꾢뜝�럥�넮");
 			mav.setViewName("/member/error");
 		}
 		
 		return mav;
 	}
 	
-	//占쎈툡占쎌뵠占쎈탵 野껓옙占쎄퉳(燁살뮄�럡占쎌뵬疫꿸퀣�삢)
+	//�뜝�럥�닡�뜝�럩逾졾뜝�럥�꺏 �뇦猿볦삕�뜝�럡�돰(�뇖�궡裕꾬옙�윞�뜝�럩逾х뼨轅명�ｏ옙�궋)
 	@RequestMapping(value="/member/mainSearchId.do",produces="text/plain;charset=utf-8")
 	@ResponseBody
 	public String mainSearchId(String id,HttpSession session)
@@ -354,7 +427,7 @@ public class MemberController {
 		return str;
 	}
 	
-	//占쎌돳占쎌뜚甕곕뜇�깈嚥∽옙 id占쎈르占쎈툡占쎄땀疫뀐옙
+	//�뜝�럩�뤂�뜝�럩�쐸�뵓怨뺣쐡占쎄퉰�슖�댙�삕 id�뜝�럥瑜닷뜝�럥�닡�뜝�럡���뼨�먯삕
 	@RequestMapping(value="/member/getId.do", produces="text/plain;charset=utf-8")
 	@ResponseBody
 	public String getIdByMno(int mno) {
--- Blank/src/main/java/com/blank/vo/LogVo.java
+++ Blank/src/main/java/com/blank/vo/LogVo.java
@@ -6,6 +6,19 @@ public class LogVo {
 	private String ip;
 	private String ldate;
 	private String id;
+	private String record;
+	public LogVo(int lno, String ip, String ldate, String id, String record) {
+		super();
+		this.lno = lno;
+		this.ip = ip;
+		this.ldate = ldate;
+		this.id = id;
+		this.record = record;
+	}
+	public LogVo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	public int getLno() {
 		return lno;
 	}
@@ -30,16 +43,11 @@ public class LogVo {
 	public void setId(String id) {
 		this.id = id;
 	}
-	public LogVo(int lno, String ip, String ldate, String id) {
-		super();
-		this.lno = lno;
-		this.ip = ip;
-		this.ldate = ldate;
-		this.id = id;
+	public String getRecord() {
+		return record;
 	}
-	public LogVo() {
-		super();
-		// TODO Auto-generated constructor stub
+	public void setRecord(String record) {
+		this.record = record;
 	}
 	
 }
